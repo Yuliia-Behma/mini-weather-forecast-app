dc64bfff1579c17c760fd7df0c9c9194955b367f
Juliia <juliyabegma@gmail.com>
2024-03-25 13:54:28 +0200

    Add weather icon from server

--- src/components/CardForTimestamp.vue
+++ src/components/CardForTimestamp.vue
@@ -1,18 +1,28 @@
 <template>
- <div class="container">
-  <div class="temperature">
-    {{ Math.round(item.main.temp) }} °C
-  </div>
-  <div class="details">
-    <div class="time-date">
-      {{ convertTimestamp(item.dt) }}
-      <img id="time-icon" src="../assets/time-icon.png" alt="time-icon" width="30">
+  <div class="container">
+    <div class="temp-icon-wraper">
+      <div class="temperature">{{ Math.round(item.main.temp) }} °C</div>
+      <img
+        id="weather-icon"
+        :src="getWeatherIcon(item.weather[0].icon)"
+        alt="weather-icon"
+      />
     </div>
-    <div class="weather-status">
-      {{ item.weather[0].description }}
+    <div class="details">
+      <div class="time-date">
+        {{ convertTimestamp(item.dt) }}
+        <img
+          id="time-icon"
+          src="../assets/time-icon.png"
+          alt="time-icon"
+          width="30"
+        />
+      </div>
+      <div class="weather-status">
+        {{ item.weather[0].description }}
+      </div>
     </div>
   </div>
- </div>
 </template>
 
 <script>
@@ -21,11 +31,26 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "CardForTimestamp",
   props: {
-    item: Object
+    item: Object,
   },
   setup() {
+    // let iconUrl = ref("");
+
     function convertTimestamp(dt) {
-      let monthes = ["Січня", "Лютого", "Березня", "Квітня", "Травня", "Червня", "Липня", "Серпня", "Вересня", "Жовтня", "Листопада", "Грудня"];
+      let monthes = [
+        "Січня",
+        "Лютого",
+        "Березня",
+        "Квітня",
+        "Травня",
+        "Червня",
+        "Липня",
+        "Серпня",
+        "Вересня",
+        "Жовтня",
+        "Листопада",
+        "Грудня",
+      ];
       let daysOfWeek = ["Нд", "Пн", "Вт", "Ср", "Чтв", "Птн", "Сб"];
       let date = new Date(dt * 1000);
       let day = date.getDate();
@@ -34,55 +59,75 @@ export default defineComponent({
       let hours = date.getHours();
       let minutes = ("0" + date.getMinutes()).substring(-2);
 
-      return `${dayOfWeek} ${day} ${month} | ${hours}:${minutes}`
-   }
-    return {
-     convertTimestamp
+      return `${dayOfWeek} ${day} ${month} | ${hours}:${minutes}`;
     }
-  }
+
+    function getWeatherIcon(iconCode) {
+      return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    }
+
+    return {
+      convertTimestamp,
+      getWeatherIcon,
+    };
+  },
 });
 </script>
 
 <style scoped>
-.container{
+.container {
   padding: 5px;
   display: flex;
   align-items: center;
   color: #2c0047;
-  background-image: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(158,255,255,0.7413340336134453) 9%, rgba(158,255,255,0.6082808123249299) 58%, rgba(255,255,255,0) 100%);
+  background-image: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0) 0%,
+    rgba(158, 255, 255, 0.7413340336134453) 9%,
+    rgba(158, 255, 255, 0.6082808123249299) 58%,
+    rgba(255, 255, 255, 0) 100%
+  );
   backdrop-filter: blur(1.5px);
   border-radius: 10px;
   border-top: 1px solid #fff;
   border-bottom: 1px solid #fff;
 }
-.temperature{
-    font-size: 2rem;
+.temperature {
+  font-size: 2rem;
   font-weight: 700;
   font-family: "Kaushan Script", cursive;
   font-weight: 400;
   font-style: normal;
-  padding-right: 10px;
-  padding: 15px;
+  padding-top: 10px;
 }
-.details{
+.details {
   padding-left: 20px;
   padding: 15px;
   color: #000;
   border-left: 1px solid #fff;
 }
-.time-date{
+.time-date {
   font-size: 1.1rem;
   font-weight: 700;
   position: relative;
 }
-.weather-status{
+.weather-status {
   font-size: 1.3rem;
   font-weight: 500;
   margin-top: 10px;
 }
-#time-icon{
+#time-icon {
   position: relative;
   right: 0;
   bottom: -5px;
+}
+#weather-icon {
+  width: 70px;
+}
+.temp-icon-wraper{
+  width: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
